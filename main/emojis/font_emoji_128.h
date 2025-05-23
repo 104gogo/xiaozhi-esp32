c68d22de9b835bd49911e5d0664cfa0895e433ee
空白泡泡糖果 <376830949@qq.com>
2025-05-23 10:08:56 +0800

    添加emoji文件

---
+++ main/emojis/font_emoji_128.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <lvgl.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// 128大小表情声明
+extern const lv_image_dsc_t emoji_1f636_128; // neutral
+extern const lv_image_dsc_t emoji_1f60a_128; // smile
+extern const lv_image_dsc_t emoji_1f622_128; // sad
+extern const lv_image_dsc_t emoji_1f620_128; // angry
+extern const lv_image_dsc_t emoji_1f62e_128; // surprise
+
+// 添加表情初始化函数声明
+const lv_font_t* font_emoji_128_init(void);
+const lv_font_t* font_emoji_64_init(void);
+const lv_font_t* font_emoji_32_init(void);
+
+#ifdef __cplusplus
+}
+#endif 
